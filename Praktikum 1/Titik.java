7fb43d55ad7701942ac17a1aefa03e673359ada4
tiaraputriw <126688819+tiaraputriw@users.noreply.github.com>
2024-02-19 11:41:11 +0700

    Add files via upload

---
+++ Praktikum 1/Titik.java
@@ -0,0 +1,36 @@
+class Titik {
+    double absis, ordinat;
+    static double counter;
+
+    Titik(){
+        absis = 0;
+        ordinat = 0;
+        counter++;
+    }
+
+    Titik(double a, double b){
+        absis = a;
+        ordinat = b;
+        counter++;
+    }
+
+    double getAbsis(){
+        return absis;
+    }
+
+    double getOrdinat(){
+        return ordinat;
+    }
+
+    static double getCounter(){
+        return counter;
+    }
+
+    void setAbsis(double a){
+        absis = a;
+    }
+
+    void setOrdinat(double b){
+        ordinat = b;
+    }
+}
